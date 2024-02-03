13461b5dbfcf61dfc745b819db889eba28ea7f49
David A. Adams <david@stemble.ca>
2024-02-03 16:25:02 -0500

    feat: return first 10 lines of text

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+#[derive(Parser)]
+struct Cli {    
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    if let Ok(lines) = read_lines(&args.path) {
+        for line in lines.flatten().take(10) {
+            println!("{}", line);
+        }
+    }
+}
+
+// Used from - https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html?search=where
+// returns an io::Result containing an iterator over lines (io::Lines) wrapped in a buffered reader (io::BufReader) for a file (File)
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    
+    // Returns an Iterator to the Reader of the lines of the file.
+    Ok(io::BufReader::new(file).lines())
+}
